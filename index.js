6216e3362c8e62e651a2bac6c955e26428555afd
mckristopher <mckristopher@gmail.com>
2020-07-14 17:21:45 +0530

    objects

--- index.js
+++ index.js
@@ -9,3 +9,18 @@ appDiv.innerHTML = `<h1>Objects</h1>`;
 let a = {}; // object literal syntax
 let b = new Object(); // object constructor syntax
 console.log(a, b);
+
+// access property value
+a.name = 'Mercy';
+console.log(a);
+
+// delete property
+delete a.name;
+console.log(a);
+
+// multword property
+let c = {
+  "super cool property": "flying"
+};
+c['super weakness'] = "candy";
+console.log(c)
